7645b22e537c1ebd2cc18dd7fba0e4d457d1bfef
Christopher Thompson <chrislegolife@gmail.com>
2015-02-11 17:09:07 -0500

    updated week3

--- 2015-Spring/workshops/week3/memory_corruption/so_meta.c
+++ 2015-Spring/workshops/week3/memory_corruption/so_meta.c
@@ -1,1 +1,22 @@
-#include <stdlib.h>#include <stdio.h>#include <unistd.h>void extractUserZip(char *userFile){    char command[1024];    snprintf(command, 1023, "unzip %s", userFile);    system(command);}int main() {    char userstring[1018];    read(0, userstring, sizeof(userstring));    extractUserZip(userstring);    return 0;}
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void extractUserZip(char *userFile) {
+    char command[1024];
+
+    snprintf(command, 1023, "unzip %s", userFile);
+
+    system(command);
+}
+
+int main() {
+    char userstring[1018];
+    
+    read(0, userstring, sizeof(userstring));
+
+    extractUserZip(userstring);
+    
+    return 0;
+}
+
