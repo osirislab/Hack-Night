01de96bf063120d5940af975e8d4de0172d898c5
Sai <svv232@nyu.edu>
2019-03-07 18:20:52 -0500

    updated angr solver

--- Rev/dynamic/angr_demo/symnorm.py
+++ Rev/dynamic/angr_demo/symnorm.py
@@ -7,26 +7,12 @@ def main():
     find = [0x40079b]
     avoid = [0x400971]
     
-    answer_chars = [claripy.BVS('input_%d' % i, 8) for i in range(25)]
-    answer = claripy.Concat(*answer_chars + [claripy.BVV(b'\n')])
+    #answer_chars = [claripy.BVS('input_%d' % i, 8) for i in range(25)]
+    #answer = claripy.Concat(*answer_chars + [claripy.BVV(b'\n')])
     
-    state = proj.factory.full_init_state(
-            args = ["./normal"],
-            stdin=answer, 
-            add_options={angr.options.LAZY_SOLVES});
-    
-    for k in answer_chars:
-        state.solver.add(k > 65)
-        state.solver.add(k < 122)
+    state = proj.factory.entry_state()
     
     sm = proj.factory.simulation_manager(state)
-    #sm.explore(find=find, avoid=avoid)
-    sm.run()
-    
-    out = b''
-    for pp in sm.deadended:
-        print(pp)
-        out = pp.posix.dumps(0)
-        print(out)
-
+    sm.explore(find=find, avoid=avoid)
+    print(sm.found[0].posix.dumps(0))
 main()
